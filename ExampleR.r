42172bea60fd94026388eb5394af8681294638ef
Alex Schiller <rnizy@osf.io>
2014-03-07 15:13:54 -0500

    Updated via the Open Science Framework

---
+++ ExampleR.r
@@ -0,0 +1,23 @@
+# Prototype
+require(osfr)
+
+?read.osf
+# read.osf {osfr}    R Documentation
+# Read osf file
+# 
+# Description
+# 
+# Create a data.frame from a file on the OSF
+
+dat <- read.osf("osf.io/api/v1/project/5ctke/osffiles/this.csv/version/1/")
+
+head(dat)
+
+# > head(dat)
+# A B C D
+# 1 1 2 3 4
+# 2 2 3 4 5
+# 3 3 4 5 6
+# 4 4 5 6 7
+# 5 5 6 7 8
+# 6 6 7 8 9
